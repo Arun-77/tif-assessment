bec3e29281f6744b06a335564c2be0d2540c3ea0
ArunKumar77 <arun.kumar@theinternetfolks.com>
2023-03-21 11:19:06 +0530

    new commit

---
+++ src/components/AssessmentHero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+interface IAssessmentHeroProps {}
+
+const AssessmentHero: React.FunctionComponent<IAssessmentHeroProps> = (
+  props
+) => {
+  return (
+    <Box>
+      <Heading>Hi</Heading>
+    </Box>
+  );
+};
+
+export default AssessmentHero;
---
+++ src/containers/AssessmentLayout.tsx
@@ -0,0 +1,12 @@
+import AssessmentHero from "@/components/AssessmentHero";
+import React from "react";
+
+interface IAssessmentLayoutProps {}
+
+const AssessmentLayout: React.FunctionComponent<IAssessmentLayoutProps> = (
+  props
+) => {
+  return <AssessmentHero />;
+};
+
+export default AssessmentLayout;
